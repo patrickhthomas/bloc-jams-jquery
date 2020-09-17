19fade97f1fbf05569dfe9c8f434bbd79ffcd5c0
Patrick <patrick@sendhelp.biz>
2020-09-17 17:04:23 -0400

    Assignment Bloc Jams Complete

--- scripts/album-data.js
+++ scripts/album-data.js
@@ -17,4 +17,6 @@ const album = {
 {
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+  $('.artist').text(album.artist);
+  $('#release-info').text(album.releaseInfo);
 }
